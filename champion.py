f6fe4ace5b9ae14ce3b98f3ea89e0309ed7178d3
Nacha Escudero Leoz <miescuderol@gmail.com>
2015-05-02 17:12:57 -0300

    checkeado. Crea todos los champs, pero sin spells ni pasiva

--- champion.py
+++ champion.py
@@ -1,11 +1,13 @@
+from decimal import *
+
 class Champion:
-    __init__(self, data):
-        stats = data['stats']
-        self.disp_name = data['name']
+    def __init__(self, data_la, data_na = None, data_eu = None):
+        stats = data_la['stats']
+        self.disp_name = data_la['name']
         self.ms = stats['movespeed']
         self.range = stats['attackrange']
         self.attack_delay = stats['attackspeedoffset']
-        self.as_base = str(Decimal(0.625 / (1 + self.attack_delay)).quantize(Decimal('1.000'))
+        self.as_base = str(Decimal(0.625 / (1 + self.attack_delay)).quantize(Decimal('1.000')))
         self.dam_base = stats['attackdamage']
         self.arm_base = stats['armor']
         self.mr_base = stats['spellblock']
@@ -21,17 +23,31 @@ class Champion:
         self.mp_lvl = stats['mpperlevel']
         self.hp5_lvl = stats['hpregenperlevel']
         self.mp5_lvl = stats['mpregenperlevel']
-        self.image = self.name.translate(None, ". ") + Square.png
-        self.title = data['title']
-        self.herotype = data['tags'][0]
-        self.alttype = str((len(data['tags']) > 1 and data['tags'][1]) or '')
-        self.rangetype = rangetype
+        self.image = self.disp_name.replace('.', '').replace(' ', '') + 'Square.png'
+        self.title = data_la['title']
+        self.herotype = data_la['tags'][0]
+        self.alttype = str((len(data_la['tags']) > 1 and data_la['tags'][1]) or '')
+        self.rangetype = ''
         self.date = ''
         self.patch = ''
-        self.health = data['info']['defense']
-        self.attack = data['info']['attack']
-        self.spells = data['info']['magic']
-        self.difficulty = data['info']['difficulty']
+        self.health = data_la['info']['defense']
+        self.attack = data_la['info']['attack']
+        self.spells = data_la['info']['magic']
+        self.difficulty = data_la['info']['difficulty']
         self.ip = ''
         self.rp = ''
-        self.passive = Passive(data['passive'])
+    #     if data_na and data_eu:
+    #         self.__spells = {'Q': Spell(data_la['spells'][0], data_na['spells'][0], data_eu['spells'][0]),
+    #                         'W': Spell(data_la['spells'][1], data_na['spells'][1], data_eu['spells'][1]),
+    #                         'E': Spell(data_la['spells'][2], data_na['spells'][2], data_eu['spells'][2]),
+    #                         'R': Spell(data_la['spells'][3], data_na['spells'][3], data_eu['spells'][3])}
+    #         self.passive = Passive(data_la['passive'], data_na['passive'], data_eu['passive'])
+    #     else:
+    #         self.__spells = {'Q': Spell(data_la['spells'][0],
+    #                         'W': Spell(data_la['spells'][1],
+    #                         'E': Spell(data_la['spells'][2],
+    #                         'R': Spell(data_la['spells'][3]}
+    #         self.passive = Passive(data_la['passive'])
+    #
+    # def get_spell(self, key):
+    #     return self.__spells[key.upper()]
--- champions_data.py
+++ champions_data.py
@@ -1,12 +1,25 @@
 import riotwatcher.riotwatcher as RiotAPI
+from champion import Champion
 
-with open('key,txt') as f:
+with open('key.txt') as f:
     key = f.read()
 
 riotApi = RiotAPI.RiotWatcher(key)
 
-static_champ_list = riotApi.static_get_champion_list(champ_data='all')
-champions_data = static_champ_list['data']
+static_champ_list_la = riotApi.static_get_champion_list(locale='es_MX', champ_data='all')
+# static_champ_list_na = riotApi.static_get_champion_list(locale='en_US', champ_data='all')
+# static_champ_list_eu = riotApi.static_get_champion_list(locale='es_ES', champ_data='all')
+champions_data_la = static_champ_list_la['data']
+# champions_data_na = static_champ_list_na['data']
+# champions_data_eu = static_champ_list_eu['data']
 
-for champ_data in champions_data:
-    champion = Champion(champ_data)
+champions = []
+for champ in champions_data_la:
+    champ_data_la = champions_data_la[champ]
+    # champ_data_na = champions_data_na[champ]
+    # champ_data_eu = champions_data_eu[champ]
+    champion = Champion(champ_data_la)
+    champions.append(champion)
+
+for champion in champions:
+    print(champion.image)
